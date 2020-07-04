3d8439b2b036f54014fe02f47a2facd5da89d654
Mert Yurt <mert-yurt@hotmail.com>
2020-07-04 19:13:57 +0300

    needs improvements

---
+++ gg-coffee-grind-project/Assets/Scripts/CoffeeBean.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CoffeeBean : MonoBehaviour
+{
+    [SerializeField]
+    GameObject smallCube;
+    [SerializeField]
+    int piecesAfterGrind = 4;
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Blade")
+        {
+            Grind();
+        }
+    }
+
+    void Grind()
+    {
+        for (int i = 0; i < piecesAfterGrind; i++)
+        {
+            Instantiate(smallCube, gameObject.transform.position, Quaternion.identity);
+        }
+        Destroy(gameObject);
+    }
+}
+
+/*GrindBlade game obj. modeli küpleri aşağı dökülmesini engelliyor. 
+Üzerine ucu yukarıya bakan bir koni ekleyerek birikmeyi önleyebilirsiniz.*/
---
+++ gg-coffee-grind-project/Assets/Scripts/PowderBox.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowderBox : MonoBehaviour
+{
+    Animator animatorP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animatorP = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown() => Shake();
+
+    void Shake()
+    {
+        animatorP.SetTrigger("isTapped");
+    }
+}
